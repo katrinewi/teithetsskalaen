477837baf12d8473a22df954a366d28dda449438
Jonas Natten <jonas.natten@knowit.no>
2024-01-31 06:21:36 +0100

    Contextual serialization of `LocalDateTime`

---
+++ src/main/kotlin/no/teithetsskalaen/model/serializers/LocalDateTimeSerializer.kt
@@ -0,0 +1,21 @@
+package no.teithetsskalaen.model.serializers
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.time.LocalDateTime
+
+@OptIn(ExperimentalSerializationApi::class)
+@Serializer(forClass = LocalDateTime::class)
+object LocalDateTimeSerializer : KSerializer<LocalDateTime> {
+    override fun serialize(encoder: Encoder, value: LocalDateTime) {
+        encoder.encodeString(value.toString())
+    }
+
+    override fun deserialize(decoder: Decoder): LocalDateTime {
+        return LocalDateTime.parse(decoder.decodeString())
+    }
+}
+
--- src/main/kotlin/no/teithetsskalaen/plugins/Json.kt
+++ src/main/kotlin/no/teithetsskalaen/plugins/Json.kt
@@ -4,12 +4,18 @@ import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import no.teithetsskalaen.model.serializers.LocalDateTimeSerializer
+import java.time.LocalDateTime
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         json(Json {
             prettyPrint = true
             isLenient = true
+            serializersModule = SerializersModule {
+                    contextual(LocalDateTime::class, LocalDateTimeSerializer)
+                }
         })
     }
 }
