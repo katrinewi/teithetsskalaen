72022301b79d2d8384f5a365d3668a4928d0b8fd
rauboti <gaute@rauboti.no>
2024-02-05 18:50:15 +0100

    basic layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,22 +1,27 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+import NavBar from './components/navigation/NavBar';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Teithetsskalaen",
-  description: "Ganske teit!",
+	title: 'Teithetsskalaen',
+	description: 'Ganske teit!',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="no">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang='no'>
+			<body className={inter.className}>
+				<NavBar />
+				{children}
+			</body>
+		</html>
+	);
 }
