048d38a0fb267d1696ad7f5c1dbf76b78e397e90
Gaute Rauboti <gaute.rauboti@knowit.no>
2024-02-06 09:19:53 +0100

    ident fixes

--- frontend/app/components/navigation/NavBar.tsx
+++ frontend/app/components/navigation/NavBar.tsx
@@ -1,14 +1,14 @@
 const NavBar = () => {
-	return (
-		<nav className="bg-gray-950 flex justify-between p-2 text-lg mb-5">
-			<div>Teithetsskalaen</div>
-			<ul className="flex justify-between gap-5">
-				<li>Home</li>
-				<li>About</li>
-			</ul>
-			<div>Logg inn</div>
-		</nav>
-	);
+  return (
+    <nav className="bg-gray-950 flex justify-between p-2 text-lg mb-5">
+      <div>Teithetsskalaen</div>
+      <ul className="flex justify-between gap-5">
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+      <div>Logg inn</div>
+    </nav>
+  );
 };
 
 export default NavBar;
