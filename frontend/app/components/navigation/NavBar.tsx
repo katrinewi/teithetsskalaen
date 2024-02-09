391d7a6e2d75ab28cfcd00cace9fb0ae2588c6d6
rauboti <gaute@rauboti.no>
2024-02-10 00:59:11 +0100

    colour palette

--- frontend/app/components/navigation/NavBar.tsx
+++ frontend/app/components/navigation/NavBar.tsx
@@ -1,6 +1,6 @@
 const NavBar = () => {
   return (
-    <nav className="bg-gray-950 flex justify-between p-2 text-lg mb-5">
+    <nav className="flex justify-between p-2 text-lg mb-5">
       <div>Teithetsskalaen</div>
       <ul className="flex justify-between gap-5">
         <li>Home</li>
