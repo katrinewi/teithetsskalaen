97a6cbb24de0a54979089a19232678f83b8bb54c
rauboti <gaute@rauboti.no>
2024-02-10 16:26:56 +0100

    Added logo and some icons

--- frontend/app/components/navigation/NavBar.tsx
+++ frontend/app/components/navigation/NavBar.tsx
@@ -1,12 +1,20 @@
+import Image from "next/image";
+import LightModeIcon from "../../assets/icons/LightModeIcon";
+import logo from "../../assets/logos/logo-dark.png";
+
 const NavBar = () => {
   return (
-    <nav className="flex justify-between p-2 text-lg mb-5">
-      <div>Teithetsskalaen</div>
-      <ul className="flex justify-between gap-5">
-        <li>Home</li>
-        <li>About</li>
-      </ul>
-      <div>Logg inn</div>
+    <nav className="flex justify-between p-2 text-lg mb-5 items-center">
+      <div className="flex justify-between gap-10 items-center">
+        <Image alt={"Teithetsskalaen"} className="w-60" src={logo} />
+        <ul className="flex justify-between gap-5">
+          <li>Home</li>
+          <li>About</li>
+        </ul>
+      </div>
+      <div className="flex justify-between gap-5 items-center">
+        <LightModeIcon fill={"white"} />
+      </div>
     </nav>
   );
 };
