f2a3ac68aae4851b30d2c5b6553560572a41c51d
Katrine Wist <katrinew0702@gmail.com>
2024-02-24 17:59:55 +0100

    Cleanup unused code

--- frontend/app/components/navigation/NavBar.tsx
+++ frontend/app/components/navigation/NavBar.tsx
@@ -9,21 +9,19 @@ const NavBar = () => {
   const { theme, setTheme } = useContext(ThemeContext);
 
   return (
-    <nav className="flex justify-end">
-      <div className="flex justify-between gap-5 items-center">
-        <IconButton
-          icon={
-            theme === "light" ? (
-              <DarkModeIcon className="fill-light-text dark:fill-dark-text" />
-            ) : (
-              <LightModeIcon className="fill-light-text dark:fill-dark-text" />
-            )
-          }
-          aria-label={`Active ${theme === "light" ? "dark" : "light"} mode`}
-          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-        />
-      </div>
-    </nav>
+    <div className="flex justify-end">
+      <IconButton
+        icon={
+          theme === "light" ? (
+            <DarkModeIcon className="fill-light-text dark:fill-dark-text" />
+          ) : (
+            <LightModeIcon className="fill-light-text dark:fill-dark-text" />
+          )
+        }
+        aria-label={`Active ${theme === "light" ? "dark" : "light"} mode`}
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      />
+    </div>
   );
 };
 
